076a1275b66084dee09876220799ed079336e37a
joaquinzxy <joaquindesosa@gmail.com>
2021-10-27 01:32:14 -0300

    flags added

--- js/init.js
+++ js/init.js
@@ -3,29 +3,31 @@
 const APIKEY = "8006a61b5a7dc0bcd14b80ea91e30cf0"
 const COUNTRIES_JSON = "js/countries.json"
 const CITIES_JSON = "js/cities.json"
+let selectedLocation = {}
 
 const countrySearchInput = document.getElementById("inputCountry")
 const citySearchInput = document.getElementById("inputCity")
+const inputFlag = document.getElementById("inputFlag")
 
 const countryMatchList = document.getElementById("countryMatchList")
 const cityMatchList = document.getElementById("cityMatchList")
+const dataContainer = document.getElementById("dataContainer")
 
 
 let countries = []
 let cities = []
 
-async function getJSONData(url){
+async function getJSONData(url) {
     return fetch(url)
-    .then(promesa=>{
-        return promesa.json()
-        .then(data=>{
-            console.log(data)
-            return data
+        .then(promesa => {
+            return promesa.json()
+                .then(data => {
+                    return data
+                })
         })
-    })
 }
 
-async function getInitData(){
+async function getInitData() {
     countries = await getJSONData(COUNTRIES_JSON)
     cities = await getJSONData(CITIES_JSON)
     cities = cities.Countries
@@ -33,62 +35,62 @@ async function getInitData(){
 
 getInitData()
 
-function countrySearch(query){
+function countrySearch(query) {
     cityMatchList.innerHTML = ""
     citySearchInput.value = ""
-
+    citySearchInput.setAttribute("disabled", "")
 
     let matches = countries.filter(country => {
         const regex = new RegExp(`^${query}`, 'gi');
         return country.name.match(regex) || country.code.match(regex)
     })
-    
-    if(query.length===0){
-        matches = []   
+
+    if (query.length === 0) {
+        matches = []
     }
 
     printCountries(matches)
 }
 
 
-function citySearch(query){
+function citySearch(query) {
     let firstMatch = cities.filter(country => {
         const regex = new RegExp(`^${countrySearchInput.value}`, 'gi');
         return country.CountryName.match(regex)
     })
     firstMatch = firstMatch[0].States
-    
-    
-    firstMatch = firstMatch.map(element=>{
-        if(element.Cities.length>0){
-            let citiesNest = element.Cities.map(e=>e)
+
+
+    firstMatch = firstMatch.map(element => {
+        if (element.Cities.length > 0) {
+            let citiesNest = element.Cities.map(e => e)
             return citiesNest
-        } else { return element.StateName}
+        } else { return element.StateName }
     }).flat()
-    
-    
+
+
     let filterMatch = firstMatch.filter(city => {
         const regex = new RegExp(`^${query}`, 'gi');
         return city.match(regex)
     })
-    
-    if(query.length===0){
-        filterMatch=[]
+
+    if (query.length === 0) {
+        filterMatch = []
     }
     printCities(filterMatch)
 }
 
-function printCountries(matches){
-    let htmlToAppend = matches.map(match=>`
-    <div class="btn card card-body mb-1" onclick="selectedCountry('${match.name}')">
-        <h6>${match.name} (${match.code}) </h6>
+function printCountries(matches) {
+    let htmlToAppend = matches.map(match => `
+    <div class="btn card card-body mb-1" onclick="selectedCountry('${match.name}','${match.code}')">
+        <h6><span class="flag-icon flag-icon-${match.code.toLowerCase()}"></span> ${match.name} (${match.code}) </h6>
     </div>
     `).join("")
     countryMatchList.innerHTML = htmlToAppend
 }
 
-function printCities(matches){
-    let htmlToAppend = matches.map(match=>`
+function printCities(matches) {
+    let htmlToAppend = matches.map(match => `
     <div class="btn card card-body mb-1" onclick="selectedCity('${match}')">
         <h6>${match}</h6>
     </div>
@@ -96,30 +98,56 @@ function printCities(matches){
     cityMatchList.innerHTML = htmlToAppend
 }
 
-
-function selectedCountry(countryName){
+function selectedCountry(countryName, countryCode) {
     countryMatchList.innerHTML = ""
-    countrySearchInput.value=countryName
+    countrySearchInput.value = countryName
+    inputFlag.innerHTML = `<span class="flag-icon flag-icon-${countryCode.toLowerCase()}"></span>`
+    citySearchInput.removeAttribute("disabled")
+    selectedLocation.country = {name: countryName, code: countryCode}
     citySearch(countryName)
 }
 
-function selectedCity(cityName){
+function selectedCity(cityName) {
     cityMatchList.innerHTML = ""
-    citySearchInput.value=cityName
+    citySearchInput.value = cityName
+    selectedLocation.city=cityName
     getWeatherData(cityName)
 }
 
-function getWeatherData(city){
-    let openWeatherQuery = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`
+async function getWeatherData(city) {
+    let openWeatherQuery = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKEY}`
     getJSONData(openWeatherQuery)
+        .then(data => {
+            if (data.cod === 200) {
+                console.log("Exito!")
+                showWeatherData(data)
+            } else {
+                console.log("Error al realizar la petición"+"("+data.cod+")")
+                showWeatherData(data, city)
+            }
+        })
 }
 
-countrySearchInput.addEventListener("input", ()=>{
+function showWeatherData(data) {
+    console.log(data)
+    let clima={
+        country: selectedLocation.country,
+        city: selectedLocation.city,
+        temp: data.main.temp,
+        status:"",
+        desc:"",
+        wind:"",
+        humidity:""
+    }
+    console.log(clima)
+}
+
+countrySearchInput.addEventListener("input", () => {
     countrySearch(countrySearchInput.value)
 
 })
 
-citySearchInput.addEventListener("input", ()=>{
+citySearchInput.addEventListener("input", () => {
     citySearch(citySearchInput.value)
 })
 
